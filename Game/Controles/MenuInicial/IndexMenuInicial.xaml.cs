e9d221dec2432c31232b0d5d4cd3b0b0be48d4a1
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-05-27 20:22:08 -0300

    Alterações em atributos gerais

--- Game/Controles/MenuInicial/IndexMenuInicial.xaml.cs
+++ Game/Controles/MenuInicial/IndexMenuInicial.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Game.Controles.TelaPadrao;
+﻿using Game.Controles.MenuInformacoesJogador;
+using Game.Controles.TelaPadrao;
 using Game.Util;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,14 @@ namespace Game.Controles.MenuInicial
         private void ComeçaJogo(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new IndexTelaPadrao());
+        }
+
+        private void NavegaInfoJogador(object sender, RoutedEventArgs e)
+        {
+            string nome = inputName.Text;
+            string life = inputLife.Text;
+            string mana = inputMana.Text;
+            this.NavigationService.Navigate(new IndexMenuInformacoesJogador(nome,life,mana));
         }
     }
 }
