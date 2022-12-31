0055774daa5fcbd77e3dc92f8d7ed16beee65050
Gabriel Teixeira <teixeirabriel@gmail.com>
2022-12-31 17:20:12 -0300

    conceito inicial do combate em equipes

---
+++ Infraestrutura/Entidades/EntCombate/Combatente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestrutura.Entidades.EntCombate
+{
+    public class Combatente : fichaBase
+    {
+        public Combatente() 
+        {
+            velAtaque = Agilidade + Energia;
+            VidaAtual = Vida;
+            ManaAtual= Mana;
+            EnergiaAtual= Energia;
+        }
+
+        public double VidaAtual { get; set; }
+        public double ManaAtual { get; set; }
+        public double EnergiaAtual { get; set; }
+        public string Foco { get; set; }
+        public string Posicao { get; set; }
+        public double velAtaque { get; set; }
+    }
+}
---
+++ Infraestrutura/Entidades/EntCombate/IdentificadorPosicaoCombatente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Infraestrutura.Entidades.EntCombate
+{
+    public class IdentificadorPosicaoCombatente
+    {
+        public string Posicao { get; set; }
+        public StackPanel Painel { get; set; }
+        public Combatente Combatente { get; set; }
+    }
+}
